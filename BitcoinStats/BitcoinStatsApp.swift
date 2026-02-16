f1c6d93865c1f8a8f3ebb5450780e15448b4b365
Jonathan Lehr <jonathan@aboutobjects.com>
2026-02-16 17:22:05 -0500

    Initial Commit

---
+++ BitcoinStats/BitcoinStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinStatsApp.swift
+//  BitcoinStats
+//
+//  Created by Jonathan Lehr on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct BitcoinStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BitcoinStats/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BitcoinStats
+//
+//  Created by Jonathan Lehr on 2/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
